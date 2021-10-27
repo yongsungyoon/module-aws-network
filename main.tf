6bee05f4d4b3465ef6f2d7c382dca8df7e12a1ee
윤용성(Yongsung Yoon)/개발혁신담당/11ST <yongsung.yoon@sk.com>
2021-10-27 23:13:56 +0900

    On adding some test configuration (book page 183)

---
+++ main.tf
@@ -0,0 +1,145 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  vpc_name = "${var.env_name} ${var.vpc_name}"
+  cluster_name = "${var.cluster_name}-${var.env_name}"
+}
+
+## Amazon VPC 정의
+resource "aws_vpc" "main" {
+  cidr_block =  var.main_vpc_cidr
+  tags = {
+    "Name" = local.vpc_name,
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+}
+
+## Subnet 정의 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public-subnet-a" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_a_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    "Name" = (
+      "${local.vpc_name}-public-subnet-a"
+    )
+    "kubernetes.io/cluster/${local.cluster.name}" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_b_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    "Name" = (
+      "${local.vpc_name}-public-subnet-b"
+    )
+    "kubernetes.io/cluster/${local.cluster.name}" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-a" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_a_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    "Name" = (
+      "${local.vpc_name}-private-subnet-a"
+    )
+    "kubernetes.io/cluster/${local.cluster.name}" = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_b_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    "Name" = (
+      "${local.vpc_name}-private-subnet-b"
+    )
+    "kubernetes.io/cluster/${local.cluster.name}" = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+## 공용 서브넷을 위한 인터넷 게이트웨이 및 라우팅 테이블
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.vpc_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public-route" {
+  vpc_id = aws_vpc.main.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${local.vpc_name}-public-route"
+  }
+}
+
+resource "aws_route_table_association" "public-a-association" {
+  subnet_id = aws_subnet.private-subnet-a.id
+  route_table_id = aws_route_table.public-route.id
+}
+
+resource "aws_route_table_association" "public-b-association" {
+  subnet_id = aws_subnet.public-subnet-b.id
+  route_table_id = aws_route_table.public-route.id
+}
+
+## NAT Gateway
+resource "aws_eip" "nat-a" {
+  vpc = true
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-a"
+  }
+}
+
+resource "aws_eip" "nat-b" {
+  vpc = true
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-b"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gw-a" {
+  allocation_id = aws_eip.nat-a.id
+  subnet_id = aws_subnet.public-subnet-a.id
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-gw-a"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gw-b" {
+  allocation_id = aws_eip.nat-b.id
+  subnet_id = aws_subnet.public-subnet-b.id
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    "Name" = "${local.vpc_name}-NAT-gw-b"
+  }
+}
